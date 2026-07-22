04a751907def3148cb72931a294622542891d014
Radha Rani <radha983576@gmail.com>
2026-07-23 01:25:15 +0530

    Prepare project for Render deployment

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.secret_key = os.getenv("SECRET_KEY", "secretkey")
 # MySQL Configuration
 
 app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT", 3306))
 app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
 app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
 app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
