8e4f8941505ee5a98210d1ed65c16ebb626b2138
Radha Rani <radha983576@gmail.com>
2026-07-23 00:48:01 +0530

    Update SQL script for cloud deployment

--- database/ems_db.sql
+++ database/ems_db.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `ems_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `ems_db`;
+USE defaultdb;
 -- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ems_db
