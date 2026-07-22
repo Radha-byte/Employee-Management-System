8e0016c5e7c5e01e3ccc5a4e5ed5661d12bd2765
Radha Rani <radha983576@gmail.com>
2026-07-23 00:55:00 +0530

    Configure project for Render deployment

--- app.py
+++ app.py
@@ -10,10 +10,15 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-app.secret_key = 'secretkey'
+app.secret_key = os.getenv("SECRET_KEY", "secretkey")
 
 # MySQL Configuration
-app.config.from_pyfile('config.py')
+
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
 
@@ -1006,5 +1011,9 @@ def logout():
     return redirect('/')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000)),
+    debug=True
+)
