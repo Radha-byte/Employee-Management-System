4d57523affdb644b42a3418d9dade620071340f9
Radha Rani <radha983576@gmail.com>
2026-07-23 01:22:15 +0530

    Track uploads directory for deployment

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+os.makedirs(app.config["UPLOAD_FOLDER"], exist_ok=True)
 
 app.secret_key = os.getenv("SECRET_KEY", "secretkey")
 
